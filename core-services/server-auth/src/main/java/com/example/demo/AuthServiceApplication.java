9c921ee42596dc173bd586aedf76823369972cd2
TriNguyen2209 <Tri.Nguyen@versapay.com>
2019-09-05 17:58:02 +0700

    Update auth server

--- core-services/server-auth/src/main/java/com/example/demo/AuthServiceApplication.java
+++ core-services/server-auth/src/main/java/com/example/demo/AuthServiceApplication.java
@@ -3,13 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
 public class AuthServiceApplication {
-	
-	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
-	}
+
+  public static void main(String[] args) {
+    SpringApplication.run(AuthServiceApplication.class, args);
+  }
 
 }
